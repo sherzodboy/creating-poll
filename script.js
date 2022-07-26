4cb0f6e8d1b95b1affeef4382dba93a5ef7a293a
sherzod02 <kholmamatovsh09>
2022-07-27 04:18:17 +0500

    realized checkbix

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const pollList = document.querySelector(".poll-list");
 const createPoll = document.querySelector(".add-list");
 const pollRadios = document.querySelector(".poll-radios");
 const body = document.querySelector(".body");
+const enteredQuestion = document.querySelector(".entered-question");
+const pollHeader = document.querySelector(".poll-header");
+const answerInput = document.querySelectorAll(".answerinput");
+const answerInputOne = document.querySelector(".answerinputone");
+const answerInputTwo = document.querySelector(".answerinputtwo");
+const answerInputThree = document.querySelector(".answerinputthree");
+const answerInputFour = document.querySelector(".answerinputfour");
+const optionText = document.querySelectorAll(".option-text");
+const optionTextOne = document.querySelector(".optiontextone");
+const optionTextTwo = document.querySelector(".optiontexttwo");
+const optionTextThree = document.querySelector(".optiontextthree");
+const optionTextFour = document.querySelector(".optiontextfour");
+
 // delete item from list
 tasks.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-xmark")) {
@@ -20,7 +33,24 @@ tasks.addEventListener("click", (e) => {
 });
 
 createPoll.addEventListener("click", () => {
-  updateInput();
+  if (enteredQuestion.value === "") {
+    alert("Type your question here");
+  } else if (
+    answerInputOne.value === "" ||
+    answerInputTwo.value === "" ||
+    answerInputThree.value === "" ||
+    answerInputFour.value === ""
+    // (answerInput.values = "")
+  ) {
+    alert("Type your answer here completely");
+  } else {
+    pollHeader.innerHTML = enteredQuestion.value;
+    optionTextOne.innerHTML = answerInputOne.value;
+    optionTextTwo.innerHTML = answerInputTwo.value;
+    optionTextThree.innerHTML = answerInputThree.value;
+    optionTextFour.innerHTML = answerInputFour.value;
+    updateInput();
+  }
 });
 
 // realize inputs
@@ -29,5 +59,5 @@ function updateInput() {
     pollList.classList.add("hidden");
     pollRadios.classList.add("hidden");
     body.classList.add("pink");
-  }, 150);
+  }, 300);
 }
